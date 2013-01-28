1b93f1c6d19a4f3dd674862c036873a279e3e5a3
Tw <tw19881113@gmail.com>
2013-01-28 10:55:38 +0800

    up install.sh

--- install.sh
+++ install.sh
@@ -5,12 +5,26 @@ if [[ ! -f install.sh ]]; then
 	exit 1
 fi
 
+#test the necessary tool
+git --version
+if [[ $? -ne 0 ]]; then
+	echo 'You must install git before install the environment' 1 >&2
+	exit 1
+fi
+hg --version
+if [[ $? -ne 0 ]]; then
+	echo 'You must install mercurial before install the environment' 1 >&2
+	exit 1
+fi
+
 BASE="$(cd ~ && pwd)"
 
 #mkdir myself dirs
 mkdir -p ~/MyRoot/bin
 mkdir -p ~/goroot
-mkdir -p ~/golib
+mkdir -p ~/golib/bin
+mkdir -p ~/golib/pkg
+mkdir -p ~/golib/src
 
 #config files
 ln -sf `pwd`/.vim ~
@@ -39,6 +53,7 @@ git co master
 make &&
 make install
 if [[ $? -ne 0 ]]; then
+	echo 'install libevent failed' 1 >&2
 	exit 1
 fi
 cd -
@@ -51,6 +66,7 @@ git co master
 make &&
 make install
 if [[ $? -ne 0 ]]; then
+	echo 'install tmux failed' 1 >&2
 	exit 1
 fi
 cd -
@@ -64,3 +80,24 @@ make;
 make install;
 cd -
 
+#change default shell
+chsh -s $BASE/MyRoot/bin/zsh
+if [[ $? -ne 0 ]]; then
+	echo 'changing default shell failed' 1 >&2
+	exit 1
+fi
+
+#go
+export GOROOT=''
+export GOPATH=''
+hg clone http://code.google.com/p/go ~/goroot &&
+cd ~/goroot/src &&
+./all.bash
+if [[ $? -ne 0 ]]; then
+	echo "install go failed" 1 >&2
+	exit 1
+fi
+cd -
+
+#use zsh
+source ~/.zshrc
