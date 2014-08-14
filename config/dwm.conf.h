59603aab32bb09070bbe9b0fea6cf2851efbd265
Tw <tw19881113@gmail.com>
2014-08-14 13:16:55 +0800

    dwm: change font to 'source code pro'
    Signed-off-by: Tw <tw19881113@gmail.com>

--- config/dwm.conf.h
+++ config/dwm.conf.h
@@ -1,7 +1,7 @@
 /* See LICENSE file for copyright and license details. */
 
 /* appearance */
-static const char font[]            = "-*-terminus-medium-r-*-*-16-*-*-*-*-*-*-*";
+static const char font[]            = "Source Code Pro:style=Regular:size=10";
 static const char normbordercolor[] = "#222222";
 static const char normbgcolor[]     = "#222222";
 static const char normfgcolor[]     = "#cccccc";
