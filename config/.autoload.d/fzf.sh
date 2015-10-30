9912a441e13cb11655281e8a4abbf405b26003e5
Tw <tw19881113@gmail.com>
2015-10-30 12:53:24 +0800

    fzf: add fshow for browsing git commits
    Signed-off-by: Tw <tw19881113@gmail.com>

--- config/.autoload.d/fzf.sh
+++ config/.autoload.d/fzf.sh
@@ -1,4 +1,18 @@
 export FZF_DEFAULT_COMMAND='pt -l -g ".*"'
 [ -f ~/.fzf.zsh ] && source ~/.fzf.zsh
 
+# fshow - git commit browser
+fshow() {
+  local out sha q
+  while out=$(
+      git log --graph --color=always \
+          --format="%C(auto)%h%d %s %C(black)%C(bold)%cr" "$@" |
+      fzf --ansi --multi --no-sort --reverse --query="$q" --print-query); do
+    q=$(head -1 <<< "$out")
+    while read sha; do
+      git show --color=always $sha | less -R
+    done < <(sed '1d;s/^[^a-z0-9]*//;/^$/d' <<< "$out" | awk '{print $1}')
+  done
+}
+
 echo "fzf init done"
