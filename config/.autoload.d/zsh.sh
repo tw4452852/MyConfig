92f668e973efa97da435b8a9b9aca7ee35e1ae04
Tw <tw19881113@gmail.com>
2016-12-08 19:59:28 +0800

    zsh: unique history entry
    Signed-off-by: Tw <tw19881113@gmail.com>

--- config/.autoload.d/zsh.sh
+++ config/.autoload.d/zsh.sh
@@ -2,6 +2,16 @@ setopt multios
 setopt correctall
 setopt extendedglob
 
+# no duplicate history
+setopt EXTENDED_HISTORY
+setopt HIST_EXPIRE_DUPS_FIRST
+setopt HIST_IGNORE_DUPS
+setopt HIST_IGNORE_ALL_DUPS
+setopt HIST_IGNORE_SPACE
+setopt HIST_FIND_NO_DUPS
+setopt HIST_SAVE_NO_DUPS
+setopt HIST_BEEP
+
 # use emacs mode by default
 bindkey -e
 
