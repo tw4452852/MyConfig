d03accf954bb144e2aab633b6979116cf31afefd
Tw <tw19881113@gmail.com>
2016-12-08 16:12:52 +0800

    zsh: make <c-o> works and add some bindkeys
    Signed-off-by: Tw <tw19881113@gmail.com>

--- config/.autoload.d/zsh.sh
+++ config/.autoload.d/zsh.sh
@@ -12,11 +12,14 @@ bindkey "\eq" push-line-or-edit
 bindkey "\e;" pound-insert
 
 # use <alt-,> for "$ANDROID_BUILD_TOP/" literally
-bindkey -s "\e," '$ANDROID_BUILD_TOP/'
+bindkey -s "\e," '$ANDROID_BUILD_TOP/out/target/product/x86_64/system'
 
-# use <ctrl-n/p> for history-substring-search
-bindkey '^P' history-substring-search-up
-bindkey '^N' history-substring-search-down
+# use <ctrl-x>p/n for history-beginning-search
+bindkey '^xp' history-beginning-search-backward
+bindkey '^xn' history-beginning-search-forward
+
+# <alt-=> for copy-prev-shell-word
+bindkey '\e=' copy-prev-shell-word
 
 # no need to surround url with single quotation
 autoload -U url-quote-magic
--- config/.zshrc
+++ config/.zshrc
@@ -33,7 +33,7 @@ zstyle :omz:plugins:ssh-agent identities id_rsa
 # Which plugins would you like to load? (plugins can be found in ~/.oh-my-zsh/plugins/*)
 # Custom plugins may be added to ~/.oh-my-zsh/custom/plugins/
 # Example format: plugins=(rails git textmate ruby lighthouse)
-plugins=(ssh-agent per-directory-history history-substring-search sudo)
+plugins=(ssh-agent per-directory-history sudo)
 
 source $ZSH/oh-my-zsh.sh
 
