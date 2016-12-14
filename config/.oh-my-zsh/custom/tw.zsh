6aa0962a61f3b2a322e1f8c2a12bad6384b5db67
Tw <tw19881113@gmail.com>
2016-12-14 10:34:20 +0800

    zsh: move myself configuration into omz's custom
    Signed-off-by: Tw <tw19881113@gmail.com>

---
+++ config/.oh-my-zsh/custom/tw.zsh
@@ -0,0 +1,159 @@
+# environment {{{
+_zdir=${ZDOTDIR:-$HOME}
+typeset -U cdpath path module_path fpath
+
+# autoload functions
+[[ -d $_zdir/.zsh/functions ]] &&
+	fpath=($_zdir/.zsh/functions $fpath) &&
+	autoload -Uz $_zdir/.zsh/functions/*(:t)
+
+# modules
+module_path=($_zdir/.zsh/modules $module_path)
+
+# exports
+export LC_ALL="en_US.UTF-8"
+
+if (( ${+commands[nvim]} )) then
+	export CSCOPE_EDITOR=nvim
+	export EDITOR=nvim
+	export MANPAGER="nvim -c 'set ft=man' -"
+elif (( ${+commands[vim]} )) then
+	export CSCOPE_EDITOR=vim
+	export EDITOR=vim
+	export MANPAGER="vim -R -"
+else
+	# just leave as it is
+fi
+
+export GOROOT=~/goroot
+export GOPATH=~/golib
+cdpath=(${GOPATH}/src ${GOROOT}/src . $cdpath)
+path=(~/MyRoot/bin ${GOROOT}/bin ${GOPATH}/bin $path)
+
+(( ${+commands[pt]} )) && export FZF_DEFAULT_COMMAND='pt -l -g ".*"'
+#}}}
+
+# options {{{
+setopt multios
+setopt extendedglob
+# regard '' as '
+setopt rc_quotes
+
+# no duplicate history
+setopt EXTENDED_HISTORY
+setopt HIST_EXPIRE_DUPS_FIRST
+setopt HIST_IGNORE_DUPS
+setopt HIST_IGNORE_ALL_DUPS
+setopt HIST_IGNORE_SPACE
+setopt HIST_FIND_NO_DUPS
+setopt HIST_SAVE_NO_DUPS
+setopt HIST_BEEP
+
+#}}}
+
+# keymaps {{{
+# use emacs mode by default
+bindkey -e
+
+# overwrite <alt-q> with push-line-or-edit
+bindkey "\eq" push-line-or-edit
+
+# use <alt-;> for commenting this line
+bindkey "\e;" pound-insert
+
+# use <alt-,> for "$ANDROID_BUILD_TOP/" literally
+bindkey -s "\e," '$ANDROID_BUILD_TOP/out/target/product/x86_64/system'
+
+# use <alt-p/n> for history-search-end
+autoload -Uz history-search-end
+zle -N history-beginning-search-backward-end \
+	history-search-end
+zle -N history-beginning-search-forward-end \
+	history-search-end
+bindkey '\ep' history-beginning-search-backward-end
+bindkey '\en' history-beginning-search-forward-end
+
+# <alt-=> for copy-earlier-word
+autoload -Uz copy-earlier-word
+zle -N copy-earlier-word
+bindkey '\e=' copy-earlier-word
+
+# no need to surround url with single quotation
+autoload -Uz url-quote-magic
+zle -N self-insert url-quote-magic
+autoload -Uz bracketed-paste-magic
+zle -N bracketed-paste bracketed-paste-magic
+
+# move by shell word
+zsh-word-movement () {
+	# see select-word-style for more
+	local -a word_functions
+	local f
+
+	word_functions=(backward-kill-word backward-word
+	capitalize-word down-case-word
+	forward-word kill-word
+	transpose-words up-case-word)
+
+	if ! zle -l $word_functions[1]; then
+		for f in $word_functions; do
+			autoload -Uz $f-match
+			zle -N zsh-$f $f-match
+		done
+	fi
+	# set the style to shell
+	zstyle ':zle:zsh-*' word-style shell
+}
+zsh-word-movement
+unfunction zsh-word-movement
+bindkey "\eB" zsh-backward-word
+bindkey "\eF" zsh-forward-word
+bindkey "\eW" zsh-backward-kill-word
+
+# <alt-j> for jump-target
+zle -N jump-target
+bindkey '\ej' jump-target
+
+# <space> for expanding abbreviation like 'iab' with Vim
+typeset -A myiabs
+myiabs=(
+	"Ig"    "| grep"
+	"Ip"    "| $PAGER"
+	"Ih"    "| head"
+	"It"    "| tail"
+	"Iv"    "| $EDITOR -R -"
+)
+my-expand-abbrev() {
+	local MATCH
+	LBUFFER=${LBUFFER%%(#m)[_a-zA-Z0-9]#}
+	LBUFFER+=${myiabs[$MATCH]:-$MATCH}
+	zle self-insert
+}
+zle -N my-expand-abbrev
+bindkey " " my-expand-abbrev
+#}}}
+
+# alias {{{
+# suffix
+alias -s pdf=zathura
+
+# directory
+hash -d A=~/code/android/5.1
+hash -d V=~/.vim
+hash -d T=~/tmp
+hash -d Z=~/.oh-my-zsh/custom
+
+#}}}
+
+# misc {{{
+# use more advanced run-help to support builtin and function
+(( ${+aliases[run-help]} )) && unalias run-help
+autoload -Uz run-help
+
+# subreap
+zmodload subreap 2>/dev/null && subreap
+
+#}}}
+
+echo "tw's zsh stuff init done"
+# vim: fdm=marker
---
+++ config/.oh-my-zsh/custom/tw.zsh-theme
@@ -0,0 +1,5 @@
+PROMPT=$'
+%{$reset_color%}%/ [%n@%m] [%T] %(1j.%j.)
+%(?..%?)> '
+
+PROMPT2="%{$fg_blod[black]%}%_> %{$reset_color%}"
