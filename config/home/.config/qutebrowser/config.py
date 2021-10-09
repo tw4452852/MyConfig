e523f93c483c4704d7736ca0eb88016fa25ba305
Tw <tw19881113@gmail.com>
2021-10-09 11:01:11 +0800

    qb: add keybind for autofill
    Signed-off-by: Tw <tw19881113@gmail.com>

--- config/home/.config/qutebrowser/config.py
+++ config/home/.config/qutebrowser/config.py
@@ -2,3 +2,5 @@ config.load_autoconfig()
 
 c.editor.command = ["editinacme", "{file}"]
 c.url.searchengines = {"DEFAULT": "https://www.google.com/search?q={}"}
+
+config.bind(',k', 'spawn --userscript qute-pass --dmenu-invocation=dmenu')
