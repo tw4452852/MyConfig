59e21c2e0154b1cc7a8916e797e6dae31c1f66fd
Tw <tw19881113@gmail.com>
2023-08-09 11:55:05 +0800

    qutebrowser: update config
    Signed-off-by: Tw <tw19881113@gmail.com>

--- .config/qutebrowser/config.py
+++ .config/qutebrowser/config.py
@@ -2,5 +2,8 @@ config.load_autoconfig()
 
 c.editor.command = ["editinacme", "{file}"]
 c.url.searchengines = {"DEFAULT": "https://www.google.com/search?q={}"}
+c.tabs.select_on_remove = "last-used"
+c.tabs.background = False
 
 config.bind(',k', 'spawn --userscript qute-pass --dmenu-invocation=dmenu')
+config.bind('aa', 'spawn -o curl -s dict://dict.org/d:{primary}:wn')
