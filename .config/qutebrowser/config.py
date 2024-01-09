d0184c820fe3ae6235ce93ee572715167234bdd2
Tw <tw19881113@gmail.com>
2024-01-09 10:28:17 +0800

    qb: use customized qute-pass
    Signed-off-by: Tw <tw19881113@gmail.com>

--- .config/qutebrowser/config.py
+++ .config/qutebrowser/config.py
@@ -5,5 +5,5 @@ c.url.searchengines = {"DEFAULT": "https://www.google.com/search?q={}"}
 c.tabs.select_on_remove = "last-used"
 c.tabs.background = False
 
-config.bind(',k', 'spawn --userscript qute-pass --dmenu-invocation=dmenu')
+config.bind(',k', 'spawn --userscript qute-pass-with-fallback --dmenu-invocation="dmenu -b"')
 config.bind('aa', 'spawn -o curl -s dict://dict.org/d:{primary}:wn')
