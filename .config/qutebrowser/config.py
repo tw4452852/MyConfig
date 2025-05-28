ba7d56b245a891873249e61d19776ff399b14b2b
Tw <tw19881113@gmail.com>
2025-05-28 09:27:07 +0800

    qb: position tabs at bottom
    Signed-off-by: Tw <tw19881113@gmail.com>
    Change-Id: I6a6a6964e4a620a861a51836db28a1c105824cb2

--- .config/qutebrowser/config.py
+++ .config/qutebrowser/config.py
@@ -4,6 +4,7 @@ c.editor.command = ["editinacme", "{file}"]
 c.url.searchengines = {"DEFAULT": "https://www.google.com/search?q={}"}
 c.tabs.select_on_remove = "last-used"
 c.tabs.background = False
+c.tabs.position = "bottom"
 
 config.bind(',k', 'spawn --userscript qute-pass-with-fallback --dmenu-invocation="dmenu -b"')
 config.bind('aa', 'spawn foot def {primary}')
