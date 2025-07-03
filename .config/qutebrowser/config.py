35f59bb13fce3015bbf7368d3a7831494c5c28a2
Tw <tw19881113@gmail.com>
2025-07-03 10:03:19 +0800

    qb: add F1 shortcut to switch proxy
    Signed-off-by: Tw <tw19881113@gmail.com>
    Change-Id: I6a6a6964bc62d1cfbcdbe774b4204606d53ca34f

--- .config/qutebrowser/config.py
+++ .config/qutebrowser/config.py
@@ -10,4 +10,5 @@ config.bind(',k', 'spawn --userscript qute-pass-with-fallback --dmenu-invocation
 config.bind('aa', 'spawn foot def {primary}')
 config.bind('<Ctrl-n>', 'completion-item-focus next', mode='command')
 config.bind('<Ctrl-p>', 'completion-item-focus prev', mode='command')
-config.bind('<F12>', 'config-cycle --temp --print content.user_stylesheets [] ["user.css"]')
+config.bind('<F12>', 'config-cycle --temp --print content.user_stylesheets ["user.css"] []')
+config.bind('<F1>', 'config-cycle --temp --print content.proxy socks://127.0.0.1:7891 system')
