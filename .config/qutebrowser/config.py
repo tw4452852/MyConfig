ca08f36cd2106f7cfa823e6f7ace9a37e8a1aacf
Tw <tw19881113@gmail.com>
2025-07-07 09:14:03 +0800

    qb: replace dmenu with fuzzel --dmenu
    Signed-off-by: Tw <tw19881113@gmail.com>
    Change-Id: I6a6a6964fad064154502e8b6b8df8a11128f92cb

--- .config/qutebrowser/config.py
+++ .config/qutebrowser/config.py
@@ -6,7 +6,7 @@ c.tabs.select_on_remove = "last-used"
 c.tabs.background = False
 c.tabs.position = "bottom"
 
-config.bind(',k', 'spawn --userscript qute-pass-with-fallback --dmenu-invocation="dmenu -b"')
+config.bind(',k', 'spawn --userscript qute-pass-with-fallback --dmenu-invocation="fuzzel --dmenu"')
 config.bind('aa', 'spawn foot def {primary}')
 config.bind('<Ctrl-n>', 'completion-item-focus next', mode='command')
 config.bind('<Ctrl-p>', 'completion-item-focus prev', mode='command')
