0009fa3447638c545e6afa419f739212de896d8e
Tw <tw19881113@gmail.com>
2025-10-20 11:12:41 +0800

    qutebrowser: switch search engine to default
    Signed-off-by: Tw <tw19881113@gmail.com>
    Change-Id: Ieec48f629254c727cfadd9294de66cc26a6a6964

--- .config/qutebrowser/config.py
+++ .config/qutebrowser/config.py
@@ -1,7 +1,5 @@
 config.load_autoconfig()
 
-c.editor.command = ["editinacme", "{file}"]
-c.url.searchengines = {"DEFAULT": "https://www.google.com/search?q={}"}
 c.tabs.select_on_remove = "last-used"
 c.tabs.background = False
 c.tabs.position = "bottom"
