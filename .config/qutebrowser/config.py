5b3931db485cbec98aa74a0244a46bfb79c83b1d
Tw <tw19881113@gmail.com>
2025-12-09 12:39:25 +0800

    qutebrowser: use gt script
    Signed-off-by: Tw <tw19881113@gmail.com>
    Change-Id: I212efc330ef2384ace8afa87167265c56a6a6964

--- .config/qutebrowser/config.py
+++ .config/qutebrowser/config.py
@@ -5,7 +5,7 @@ c.tabs.background = False
 c.tabs.position = "bottom"
 
 config.bind(',k', 'spawn --userscript qute-pass-with-fallback --dmenu-invocation="fuzzel --dmenu"')
-config.bind('aa', 'spawn foot --log-level=error --app-id=_float def {primary}')
+config.bind('aa', 'spawn gt def {primary}')
 config.bind('<Ctrl-n>', 'completion-item-focus next', mode='command')
 config.bind('<Ctrl-p>', 'completion-item-focus prev', mode='command')
 config.bind('<F12>', 'config-cycle --temp --print content.user_stylesheets ["user.css"] []')
